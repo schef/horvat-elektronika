8d9a21b115b83fc842a5fdf227e4adcb56eb0468
schef <zvanstefan@gmail.com>
2014-10-17 13:45:41 +0200

    added author

--- source/scripts/imenik.py
+++ source/scripts/imenik.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python
 
+"""
+Telefonski imenik
+Location: https://github.com/schef/horvat-elektronika/source/scripts
+"""
+
+__author__ = 'Stjepan Horvat (zvanstefan@gmail.com)'
+__copyright__ = 'Copyright (c) 2014 Stjepan Horvat'
+__license__ = 'GNU'
+__vcs_id__ = '$Id$'
+__version__ = '1.0'
+
+import sys
+from argparse import ArgumentParser
 import requests #used for POST request
 from bs4 import BeautifulSoup #used form html parsing
 from colorama import Fore #used form colouring
+
+ap = ArgumentParser()
+ap.add_argument('-t', '--tko', type=str, help="WHO are you searching for")
+ap.add_argument('-g', '--gdje', type=str, help="WHERE are you searching from")
+args = ap.parse_args()
 
 #prints the welcome message
 print("Telefonski imenik")
 
 #TODO:if no argument are added it asks you for the input
-tko_input = input("Tko: ")
-gdje_input = input("Gdje: ")
+if (args.tko):
+    tko_input = args.tko
+else:
+    tko_input = input("Tko: ")
+if (args.gdje):
+    gdje_input = args.gdje
+else:
+    gdje_input = input("Gdje: ")
 
 #submit form defined by the webpage coder
 url = 'http://imenik.tportal.hr'
@@ -21,6 +45,7 @@ data = {
     "gdje" : gdje_input
 }
 
+print("Searching...")
 r = requests.post(url, data)
 
 #TODO:save if user arg required it
@@ -53,15 +78,20 @@ for tag in soup.find_all(["div", "li"], class_=["resultsTitle", "secondColumn", 
         if ['resultsTitle'] in (tag.attrs.values()):
             lista[index].append(tag.contents[0])
 
-#print adresses found from list
-for i in range(len(lista)):
-    print("[", Fore.GREEN + str(i) + Fore.RESET, "] ", Fore.BLUE + lista[i][0] + Fore.RESET, " ", lista[i][1], " ", lista[i][2], " ", lista[i][3], sep='')
 
-#TODO:check the list and copy the number to xclip
+#TODO:check the list and copy the number to xclip if requred by user by args
 if len(lista) == 0:
         print("Nothing found!")
+        sys.exit()
 elif len(lista) == 1:
-        print(lista[0][3])
+    #print the first and only element
+    print("Only 1 item found:")
+    print("[", Fore.GREEN + str(1) + Fore.RESET, "] ", Fore.BLUE + lista[0][0] + Fore.RESET, " ", lista[0][1], " ", lista[0][2], " ", lista[0][3], sep='')
+    print(lista[0][3])
 elif len(lista) > 1:
-        user_input = input("Some input please: ")
-        print(lista[int(user_input)][3])
+    #print adresses found from list
+    print("Found", len(lista), "items:")
+    for i in range(len(lista)):
+        print("[", Fore.GREEN + str(i) + Fore.RESET, "] ", Fore.BLUE + lista[i][0] + Fore.RESET, " ", lista[i][1], " ", lista[i][2], " ", lista[i][3], sep='')
+    user_input = input("Some input please: ")
+    print(lista[int(user_input)][3])
